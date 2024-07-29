2185b22a0baf54947e79dfdcd7b1eb85911f5a73
SoniiPP <sonypoudel30@gmail.com>
2024-07-30 00:20:17 +1000

    some changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
-import { getImageUrl } from "../../utilis";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
       localsConvention: "camelCase"
     },
   },
+  resolve: {
+    alias: {
+      '@': '/src', // Simplifies imports from the src directory
+    },
+  },
+  build: {
+    assetsInlineLimit: 4096, // Adjust this limit if needed
+  },
+  base: './', // Ensures the base path is correctly set for your deployment
+  server: {
+    open: true, // Automatically open the app in the browser on server start
+  },
 });
