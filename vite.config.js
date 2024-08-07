644047da577139a0887d366afda3ae1b46721704
SoniiPP <sonypoudel30@gmail.com>
2024-08-08 02:30:52 +1000

    updated json files and images

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,9 @@ export default defineConfig({
     modules: {
       localsConvention: "camelCase"
     },
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Simplifies imports from the src directory
-    },
-  },
-  build: {
-    assetsInlineLimit: 4096, // Adjust this limit if needed
-  },
-  base: './', // Ensures the base path is correctly set for your deployment
-  server: {
-    open: true, // Automatically open the app in the browser on server start
-  },
-});
+    server: {
+      port: 3000,
+      open: true
+    }
+  }
+})
